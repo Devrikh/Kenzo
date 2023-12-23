d47c97a3ca0ae5c56a15f5c71ad39ce0091435c5
Devrikh Jatav <vjatav997@gmail.com>
2023-12-23 22:04:27 +0530

    Server File delete

--- Server/Kenzo/Routers/user.route.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const UserController = require('../Controller/user.controller');
-router.post('/signin', UserController.register);
-router.post("/login", UserController.login);
-router.post("/profile", UserController.getProfile);
-module.exports = router;
--- Server/Kenzo/app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const UserRouter = require('./Routers/user.route');
-const app = express();
-app.use(express.json());
-app.use('/', UserRouter);
-module.exports = app;
